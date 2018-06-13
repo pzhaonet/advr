b28ff6b3b20a32d53f7e7d2d42a0b44cd4f606a9
dapeng <baydap@gmail.com>
2018-06-13 23:34:42 +0200

    bug fixed

--- R/advr.r
+++ R/advr.r
@@ -1,188 +1,3 @@
-################ Load data and functions ######
-# path_raw <- paste0(wd, '/advection_MB/data_raw/adv/CR1000_AdvData_2015_MB.dat')
-# path_raw_header <- paste0(wd, '/advection_MB/data_raw/adv/header.csv')
-#' Visualization of the raw data
-#'
-#' @param x data frame of the raw data
-#'
-#' @return images of the raw data
-#' @export
-#'
-#' @examples
-plotraw <- function(x){
-  layout(matrix(c(1:10), 5, 2, byrow = FALSE),heights=c(1,1,1,1,1))
-  par(cex = 1, mar = c(2,4,0.5,1)) #cex = 0.8,
-
-  # pressure
-  print(paste(Sys.time(), 'plotting P'))
-  plot(x$TIMESTAMP, x[,'Pressure_Avg'], type='l', lwd=1.5, col='blue', ylab='Air Pressure')
-  abline(h = seq(700, 1000, 50), col = 'grey')
-
-  # Temperature
-  print(paste(Sys.time(), 'plotting t'))
-  mycol <- rainbow(length(colname_t))
-  plot(x$TIMESTAMP, x[, colname_t[1]], col=mycol[1], ylab='T', type = 'p', pch = 20, cex = 0.5, ylim = lim_t, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels) # It worked before. But now it is not working.
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(h = seq(-15, 30, 5), v = as.numeric(xat), col = 'grey')
-  for (m in 2:length(colname_t)){
-    points(x$TIMESTAMP, x[, colname_t[m]], col=mycol[m], ylab='T', type = 'p', pch = 20, cex = 0.5) #, type='l',lwd=1.5)
-  }
-  legend('topright', bty = 'n', col = mycol, legend = colname_t, lty = 1)
-
-  # RH
-  print(paste(Sys.time(), 'plotting RH'))
-  mycol <- rainbow(length(colname_t))
-  plot(x$TIMESTAMP, x[, colname_rh[1]], col=mycol[1], ylab='RH', type = 'p', pch = 20, cex = 0.5, ylim = lim_rh, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(0, 110))
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(h = seq(0,100,20), v = as.numeric(xat), col = 'grey')
-  for (m in 1:length(colname_rh)){
-    points(x$TIMESTAMP, x[, colname_rh[m]], col=mycol[m], type = 'p', pch = 20, cex = 0.5) #, type='l',lwd=1.5)
-  }
-
-  # wind speed
-  print(paste(Sys.time(), 'plotting WS'))
-  mycol <- rainbow(length(colname_ws))
-  plot(x$TIMESTAMP, x[, colname_ws[1]], col = mycol[1], ylab='Wind Speed', pch = 20, cex = 0.5, type = 'p', ylim = lim_ws, axes = FALSE)# ,type='l' ) #, ylim = c(0, 6))
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(h = seq(0, 20, 1), v = as.numeric(xat), col = 'grey')
-  for (m in 2:length(colname_ws)){
-    points(x$TIMESTAMP, x[, colname_ws[m]], cex = 0.5, type = 'p', pch = 20, col=mycol[m]) # type='l',
-  }
-  legend('topright', bty = 'n', col = mycol, legend = colname_wd, lty = 1)
-
-  # wind direction
-  print(paste(Sys.time(), 'plotting WD'))
-  plot(x$TIMESTAMP, x[,colname_wd[1]], type='p', cex = 0.5, col = mycol[1], ylab='Wind dir', ylim = lim_wd, axes = FALSE) #c(0,360))
-  axis(2)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  box()
-  abline(h = seq(0, 360, 45), v = as.numeric(xat), col = 'grey')
-  for (m in 2:length(colname_wd)){
-    points(x$TIMESTAMP, x[, colname_wd[m]], type='p',cex = 0.5, col=mycol[m])
-  }
-
-  # valves
-  print(paste(Sys.time(), 'plotting valves'))
-  plot(x$TIMESTAMP, x$vFlushing, type='l', lwd=1, col='red', ylab='valve number', ylim = lim_valve, axes = FALSE)
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(v = as.numeric(xat), col = 'grey')
-  points(x$TIMESTAMP, x$vSampling, type='l',lwd=1,col='blue')
-  legend('topright', bty = 'n', col = c('red', 'blue'), legend = c('flushing', 'sampling'), lty = 1)
-
-  # flowrate
-  print(paste(Sys.time(), 'plotting flow rate'))
-  plot(x$TIMESTAMP, x$flowrate_m, type='l', lwd=1, col='red', ylab='flowrate', ylim = lim_flow, axes = FALSE)
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(h = seq(0, 2, 0.5), v = as.numeric(xat), col = 'grey')
-  points(x$TIMESTAMP, x$flowrate_f, type='l',lwd=1, col='blue')
-  legend('topright', bty = 'n', col = c('red', 'blue'), legend = c('flushing', 'sampling'), lty = 1)
-
-  # CO2
-  print(paste(Sys.time(), 'plotting CO2'))
-  plot(x$TIMESTAMP, x[,'irga_co2_Avg'], type = 'p', pch = 20, cex = 0.5, col='red', ylab='CO2', ylim = lim_co2, axes = FALSE) #, type='l', lwd=1, ylim = c(0, 900))
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(v = as.numeric(xat), col = 'grey')
-  box()
-
-  # H2O
-  print(paste(Sys.time(), 'plotting H2O'))
-  plot(x$TIMESTAMP, x[,'irga_h2o_Avg'], type = 'p', pch = 20, cex = 0.5, col='blue', ylab='H2O', ylim = lim_h2o, axes = FALSE) #type='l', lwd=1.5,  ylim = c(0, 60))
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(h = seq(0), v = as.numeric(xat), col = 'grey')
-
-  # Title
-  plot.new()
-  text(x=0.5, y=0.5, paste('Advection\n', as.Date(d, origin = "1970-01-01"), sep = ''),cex=2.5)
-  par(mar=c(5,9,4.6,3),cex.lab=3,cex.axis=3,cex.main=3,xpd=F)
-}
-
-#' Visualization of the temperature data separately
-#'
-#' @param x data frame of the raw data
-#'
-#' @return images of the raw temperature data in sub figures
-#' @export
-#'
-#' @examples
-plotraw_t <- function(x){
-  layout(matrix(c(1:(ceiling((length(colname_t) + 1) /2) * 2)), ceiling((length(colname_t) + 1)/2), 2, byrow = FALSE))
-  par(cex = 1, mar = c(2,4,0.5,1)) #cex = 0.8,
-  mycol <- rainbow(length(colname_t))
-  plot(x$TIMESTAMP, x[, colname_t[1]], col=mycol[1], ylab='T', type = 'p', pch = 20, cex = 0.5, ylim = lim_t, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(h = seq(-15, 30, 5), v = as.numeric(xat), col = 'grey')
-  for (m in 2:length(colname_t)){
-    points(x$TIMESTAMP, x[, colname_t[m]], col=mycol[m], ylab='T', type = 'p', pch = 20, cex = 0.5) #, type='l',lwd=1.5)
-  }
-  for (m in 1:length(colname_t)){
-    plot(x$TIMESTAMP, x[, colname_t[m]], col=mycol[m], ylab='T', type = 'p', pch = 20, cex = 0.5, ylim = lim_t, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
-    axis(2)
-    # axis.POSIXct(1, at = xat, labels = xlabels)
-    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-    box()
-    abline(h = seq(-15, 30, 5), v = as.numeric(xat), col = 'grey')
-    legend('topright', bty = 'n', col = mycol[m], legend = colname_t[m], lty = 1)
-  }
-}
-
-#' Visualization of the relative humidity data separately
-#'
-#' @param x data frame of the raw data
-#'
-#' @return images of the raw relative humidity data in sub figures
-#' @export
-#'
-#' @examples
-plotraw_rh <- function(x){
-  layout(matrix(c(1:(ceiling((length(colname_rh) + 1)/2) * 2)), ceiling((length(colname_rh) + 1)/2), 2, byrow = FALSE))
-  par(cex = 1, mar = c(2,4,0.5,1)) #cex = 0.8,
-  mycol <- rainbow(length(colname_rh))
-
-  plot(x$TIMESTAMP, x[, colname_rh[1]], col=mycol[1], ylab='RH', type = 'p', pch = 20, cex = 0.5, ylim = lim_rh, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
-  axis(2)
-  # axis.POSIXct(1, at = xat, labels = xlabels)
-  axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-  box()
-  abline(h = seq(0,100,20), v = as.numeric(xat), col = 'grey')
-  for (m in 2:length(colname_rh)){
-    points(x$TIMESTAMP, x[, colname_rh[m]], col=mycol[m], ylab='T', type = 'p', pch = 20, cex = 0.5) #, type='l',lwd=1.5)
-  }
-
-  for (m in 1:length(colname_rh)){
-    plot(x$TIMESTAMP, x[, colname_rh[m]], col=mycol[m], ylab='RH', type = 'p', pch = 20, cex = 0.5, ylim = lim_rh, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
-    axis(2)
-    # axis.POSIXct(1, at = xat, labels = xlabels)
-    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
-    box()
-    abline(h = seq(0,100,20), v = as.numeric(xat), col = 'grey')
-    legend('topright', bty = 'n', col = mycol[m], legend = colname_rh[m], lty = 1)
-  }
-}
 
 #' Read and pre process the raw data
 #'
@@ -193,6 +8,10 @@ plotraw_rh <- function(x){
 #' @export
 #'
 #' @examples
+#' if(!dir.exists('demo')) dir.create('demo')
+#' download.file('https://github.com/pzhaonet/advr/raw/master/inst2/data.zip', 'demo/data.zip')
+#' unzip(zipfile = 'demo/data.zip', exdir = 'demo')
+#' adv_data <- read_adv('demo/data_sample.csv', 'demo/data_header.csv')
 read_adv <- function(path_raw, path_raw_header){
   aa <- read.table(file = path_raw, header = FALSE, sep = ',', skip = 4, stringsAsFactors = FALSE, na.strings = c('NA', 'NaN', 'NAN'))
   header  <- read.table(file = path_raw_header, header = FALSE, sep = ',', stringsAsFactors = FALSE)
@@ -202,6 +21,7 @@ read_adv <- function(path_raw, path_raw_header){
   aa$flowrate_f  <- aa$flowvolt_f_Avg/ 5000 * 20
   aa$cdry <- aa$irga_co2_Avg / (1-aa$irga_h2o_Avg/1000)
   aa$wdry <- aa$irga_h2o_Avg / (1-aa$irga_h2o_Avg/1000)
+  return(aa)
 }
 
 #' plot raw data in day, hour, or other length
@@ -212,16 +32,173 @@ read_adv <- function(path_raw, path_raw_header){
 #'
 #' @return figures in day, hour, or other length
 #' @export
-#'
-#' @examples
-plotraw_cut <- function(aa, sub_by = 1, suffix = 'daily'){
+plot_adv <- function(aa, sub_by = 1, suffix = 'daily'){
+  plotraw <- function(x){
+    layout(matrix(c(1:10), 5, 2, byrow = FALSE),heights=c(1,1,1,1,1))
+    par(cex = 1, mar = c(2,4,0.5,1)) #cex = 0.8,
+
+    # pressure
+    print(paste(Sys.time(), 'plotting P'))
+    plot(x$TIMESTAMP, x[,'Pressure_Avg'], type='l', lwd=1.5, col='blue', ylab='Air Pressure')
+    abline(h = seq(700, 1000, 50), col = 'grey')
+
+    # Temperature
+    print(paste(Sys.time(), 'plotting t'))
+    mycol <- rainbow(length(colname_t))
+    plot(x$TIMESTAMP, x[, colname_t[1]], col=mycol[1], ylab='T', type = 'p', pch = 20, cex = 0.5, ylim = lim_t, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels) # It worked before. But now it is not working.
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(h = seq(-15, 30, 5), v = as.numeric(xat), col = 'grey')
+    for (m in 2:length(colname_t)){
+      points(x$TIMESTAMP, x[, colname_t[m]], col=mycol[m], ylab='T', type = 'p', pch = 20, cex = 0.5) #, type='l',lwd=1.5)
+    }
+    legend('topright', bty = 'n', col = mycol, legend = colname_t, lty = 1)
+
+    # RH
+    print(paste(Sys.time(), 'plotting RH'))
+    mycol <- rainbow(length(colname_t))
+    plot(x$TIMESTAMP, x[, colname_rh[1]], col=mycol[1], ylab='RH', type = 'p', pch = 20, cex = 0.5, ylim = lim_rh, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(0, 110))
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(h = seq(0,100,20), v = as.numeric(xat), col = 'grey')
+    for (m in 1:length(colname_rh)){
+      points(x$TIMESTAMP, x[, colname_rh[m]], col=mycol[m], type = 'p', pch = 20, cex = 0.5) #, type='l',lwd=1.5)
+    }
+
+    # wind speed
+    print(paste(Sys.time(), 'plotting WS'))
+    mycol <- rainbow(length(colname_ws))
+    plot(x$TIMESTAMP, x[, colname_ws[1]], col = mycol[1], ylab='Wind Speed', pch = 20, cex = 0.5, type = 'p', ylim = lim_ws, axes = FALSE)# ,type='l' ) #, ylim = c(0, 6))
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(h = seq(0, 20, 1), v = as.numeric(xat), col = 'grey')
+    for (m in 2:length(colname_ws)){
+      points(x$TIMESTAMP, x[, colname_ws[m]], cex = 0.5, type = 'p', pch = 20, col=mycol[m]) # type='l',
+    }
+    legend('topright', bty = 'n', col = mycol, legend = colname_wd, lty = 1)
+
+    # wind direction
+    print(paste(Sys.time(), 'plotting WD'))
+    plot(x$TIMESTAMP, x[,colname_wd[1]], type='p', cex = 0.5, col = mycol[1], ylab='Wind dir', ylim = lim_wd, axes = FALSE) #c(0,360))
+    axis(2)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    box()
+    abline(h = seq(0, 360, 45), v = as.numeric(xat), col = 'grey')
+    for (m in 2:length(colname_wd)){
+      points(x$TIMESTAMP, x[, colname_wd[m]], type='p',cex = 0.5, col=mycol[m])
+    }
+
+    # valves
+    print(paste(Sys.time(), 'plotting valves'))
+    plot(x$TIMESTAMP, x$vFlushing, type='l', lwd=1, col='red', ylab='valve number', ylim = lim_valve, axes = FALSE)
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(v = as.numeric(xat), col = 'grey')
+    points(x$TIMESTAMP, x$vSampling, type='l',lwd=1,col='blue')
+    legend('topright', bty = 'n', col = c('red', 'blue'), legend = c('flushing', 'sampling'), lty = 1)
+
+    # flowrate
+    print(paste(Sys.time(), 'plotting flow rate'))
+    plot(x$TIMESTAMP, x$flowrate_m, type='l', lwd=1, col='red', ylab='flowrate', ylim = lim_flow, axes = FALSE)
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(h = seq(0, 2, 0.5), v = as.numeric(xat), col = 'grey')
+    points(x$TIMESTAMP, x$flowrate_f, type='l',lwd=1, col='blue')
+    legend('topright', bty = 'n', col = c('red', 'blue'), legend = c('flushing', 'sampling'), lty = 1)
+
+    # CO2
+    print(paste(Sys.time(), 'plotting CO2'))
+    plot(x$TIMESTAMP, x[,'irga_co2_Avg'], type = 'p', pch = 20, cex = 0.5, col='red', ylab='CO2', ylim = lim_co2, axes = FALSE) #, type='l', lwd=1, ylim = c(0, 900))
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(v = as.numeric(xat), col = 'grey')
+    box()
+
+    # H2O
+    print(paste(Sys.time(), 'plotting H2O'))
+    plot(x$TIMESTAMP, x[,'irga_h2o_Avg'], type = 'p', pch = 20, cex = 0.5, col='blue', ylab='H2O', ylim = lim_h2o, axes = FALSE) #type='l', lwd=1.5,  ylim = c(0, 60))
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(h = seq(0), v = as.numeric(xat), col = 'grey')
+
+    # Title
+    plot.new()
+    text(x=0.5, y=0.5, paste('Advection\n', as.Date(d, origin = "1970-01-01"), sep = ''),cex=2.5)
+    par(mar=c(5,9,4.6,3),cex.lab=3,cex.axis=3,cex.main=3,xpd=F)
+  }
+
+  plotraw_t <- function(x){
+    layout(matrix(c(1:(ceiling((length(colname_t) + 1) /2) * 2)), ceiling((length(colname_t) + 1)/2), 2, byrow = FALSE))
+    par(cex = 1, mar = c(2,4,0.5,1)) #cex = 0.8,
+    mycol <- rainbow(length(colname_t))
+    plot(x$TIMESTAMP, x[, colname_t[1]], col=mycol[1], ylab='T', type = 'p', pch = 20, cex = 0.5, ylim = lim_t, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(h = seq(-15, 30, 5), v = as.numeric(xat), col = 'grey')
+    for (m in 2:length(colname_t)){
+      points(x$TIMESTAMP, x[, colname_t[m]], col=mycol[m], ylab='T', type = 'p', pch = 20, cex = 0.5) #, type='l',lwd=1.5)
+    }
+    for (m in 1:length(colname_t)){
+      plot(x$TIMESTAMP, x[, colname_t[m]], col=mycol[m], ylab='T', type = 'p', pch = 20, cex = 0.5, ylim = lim_t, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
+      axis(2)
+      # axis.POSIXct(1, at = xat, labels = xlabels)
+      axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+      box()
+      abline(h = seq(-15, 30, 5), v = as.numeric(xat), col = 'grey')
+      legend('topright', bty = 'n', col = mycol[m], legend = colname_t[m], lty = 1)
+    }
+  }
+
+  plotraw_rh <- function(x, ...){
+    layout(matrix(c(1:(ceiling((length(colname_rh) + 1)/2) * 2)), ceiling((length(colname_rh) + 1)/2), 2, byrow = FALSE))
+    par(cex = 1, mar = c(2,4,0.5,1)) #cex = 0.8,
+    mycol <- rainbow(length(colname_rh))
+
+    plot(x$TIMESTAMP, x[, colname_rh[1]], col=mycol[1], ylab='RH', type = 'p', pch = 20, cex = 0.5, ylim = lim_rh, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
+    axis(2)
+    # axis.POSIXct(1, at = xat, labels = xlabels)
+    axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+    box()
+    abline(h = seq(0,100,20), v = as.numeric(xat), col = 'grey')
+    for (m in 2:length(colname_rh)){
+      points(x$TIMESTAMP, x[, colname_rh[m]], col=mycol[m], ylab='T', type = 'p', pch = 20, cex = 0.5) #, type='l',lwd=1.5)
+    }
+
+    for (m in 1:length(colname_rh)){
+      plot(x$TIMESTAMP, x[, colname_rh[m]], col=mycol[m], ylab='RH', type = 'p', pch = 20, cex = 0.5, ylim = lim_rh, axes = FALSE)# type='l', lwd=1.5) #, ylim =c(-5, 25))
+      axis(2)
+      # axis.POSIXct(1, at = xat, labels = xlabels)
+      axis.POSIXct(1, x$TIMESTAMP, at = xat, labels = xlabels)
+      box()
+      abline(h = seq(0,100,20), v = as.numeric(xat), col = 'grey')
+      legend('topright', bty = 'n', col = mycol[m], legend = colname_rh[m], lty = 1)
+    }
+  }
+
+  if(!dir.exists('output')) dir.create('output')
   if(is.na(sub_by)) {
     sub_by <- as.numeric(diff(date_range)) + 1 # the whole dataset
     suffix <- 'all'
   }
 
   ###### settings
-  colname_t[!colname_t %in% names(aa)]
   lim_t <- range(aa[, colname_t], na.rm = TRUE)
   colname_rh <- c(paste('hcRH_Avg(', 1:8, ')', sep = ''))
   lim_rh <- range(aa[, colname_rh], na.rm = TRUE)
@@ -271,4 +248,26 @@ plotraw_cut <- function(aa, sub_by = 1, suffix = 'daily'){
       dev.off()
     }
   }
+}
+
+#' Download the logger program
+#'
+#' @return a .CR1 program file
+#' @export
+#'
+#' @examples
+#' get_logger()
+get_logger <- function(){
+ download.file('https://github.com/pzhaonet/advr/raw/master/inst2/logger.CR1', 'logger.CR1')
+}
+
+#' Download an Arduino program file
+#'
+#' @return an .ino file
+#' @export
+#'
+#' @examples
+#' get_arduino()
+get_arduino <- function(){
+  download.file('https://github.com/pzhaonet/advr/raw/master/inst2/arduino.ino', 'arduino.ino')
 }
